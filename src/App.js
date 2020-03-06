f6b0282e6137c2cec32222f0ce1cee87387ba058
alexvancap <alexvancap@gmail.com>
2020-03-06 11:11:07 +0100

    react set-up

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, //Route, 
+  Switch } from "react-router-dom";
+import './App.css';
+import history from './components/history';
+
+function App() {
+  return (
+    <Router history={history}>
+      <div className="App">
+      
+      </div>
+      <Switch>
+          {/* <Route path="/home">
+            <About />
+          </Home> */}
+        </Switch>
+    </Router>
+  );
+}
+
+export default App;
